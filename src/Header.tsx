d83b7aa9296da8dab3a612023f84fdf8867922e9
Christian dela Cruz <66873179+ctdc0208@users.noreply.github.com>
2024-01-16 22:38:42 +0800

    install tailwindcss

---
+++ src/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <div>
+            <div className="text-3xl font-bold underline">NBA Today</div>
+            <div>Buttons</div>
+        </div>
+    )
+}
