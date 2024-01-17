620b503239679421423844e764a08fd52c351af7
Christian dela Cruz <66873179+ctdc0208@users.noreply.github.com>
2024-01-17 08:32:26 +0800

    initial header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
-/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blue': '#01579b',
+        'bg-black': '#171717',
+        'bg-card-black': '#202124',
+        'font-white-game' : '#bdc1c6',
+      }
+    }
   },
   plugins: [],
 }
